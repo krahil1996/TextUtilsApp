b106a5f7015ac19d82e394f4bb63562d4239e209
krahil1996 <rahil393@gmail.com>
2024-10-16 17:20:14 +0530

    update files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo_light.png'
 import PropTypes from 'prop-types'
 
 
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-dark border-bottom">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    <img src={logo} alt={props.title} width="40" height="30" /> <b>{props.title}</b>
+                <a className="navbar-brand text-light" href="/">
+                    <img src={logo} alt={props.title} width="32" height="32" /> <b>{props.title}</b>
                 </a>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="/">Home</a>
+                            <a className="nav-link text-light" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.aboutText}</a>
+                            <a className="nav-link text-light" href="/">{props.aboutText}</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
+                    {/* <form className="d-flex" role="search">
                         <input className="form-control me-1" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,48 +7,33 @@ import React, { useState } from 'react';
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter text here.');
+    const [text, setText] = useState('');
     // text = "new text ", //wrong way to change state
     // setText("new text"); // correct way to change the state
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText)
-        // console.log(`Entered text: ${text} converted into ${newText}`);
 
     }
     const handleloClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
-        // console.log(`Entered text: ${text} converted into ${newText}`);
 
     }
     const handlesenClick = () => {
-        if (text === null) {
-            return false;
-        }
-        else {
-            text.replace(/\w\S*/g,
-                function (txt) {
-                    return txt.charAt(0).toUpperCase() +
-                        txt.substring(1).toLowerCase();
-                }
 
-            )
-        }
-
-        // console.log(`Entered text: ${text} converted into ${newText}`);
 
     }
     const handleOnChange = (e) => {
-        // console.log("on Change");
         setText(e.target.value)
-
     }
     const resetText = () => {
-        setText('Enter text here.');
+        let newText = "";
+        setText(newText)
     }
-    let words = text.trimStart().split(" ").length;
-    console.log(text.trimStart().split(" ").length);
+    let words = text.trimStart(" ").split(" ").length;
+    let lines = text.split("\r\n|\r|\n").length;
+    console.log(words);
     return (
         <>
             <div>
@@ -58,9 +43,12 @@ export default function TextForm(props) {
                         onClick={resetText}
                         className=" mt-3 btn btn-warning float-lg-end my-2">Reset Text</button>
                     <textarea
-                        className="form-control mb-2" style={{ resize: 'initial' }} id="myBox" rows="14"
+                        placeholder='Enter Text here.'
+                        className="form-control mb-2 text-wrap" style={{ resize: 'initial' }} id="myBox" rows="14"
                         onChange={handleOnChange}
-                        value={text} >Rahil-Khan</textarea>
+                        value={text} >
+
+                    </textarea>
                     <button onClick={handleUpClick} className="btn btn-primary m-2">Convert to Uppercase</button>
                     <button onClick={handleloClick} className="btn btn-primary m-2">Convert to Lowercase</button>
                     <button onClick={handlesenClick} className="btn btn-primary m-2">Convert to Sentence Case</button>
@@ -72,11 +60,26 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1">Convert to Uppercase</button> */}
             </div >
             <div className="container">
-                <h1>Your text summary</h1>
-                <p>{words} words and {text.length} Characters</p>
+                <h2>Your text summary</h2>
+
+                <div className='d-flex flex-wrap gap-2 flex-row align-items-center justify-content-evenly w-50 text-light bg-danger rounded-1 p-1  mb-1'>
+                    <div className='p-1  '>
+                        <span className='badge bg-dark rounded-circle text-center'>{words}</span><p className='px-1 d-inline'>Words</p>
+                    </div>
+                    <span className='border-2 border-end border-light border-opacity-75 text-danger'>{"."}</span>
+                    <div className='p-1 '>
+                        <span className='badge bg-dark rounded-circle text-center '>{text.length}</span><p className='px-1 d-inline'>Characters</p>
+                    </div>
+                    <span className='border-2 border-end border-light text-danger  border-opacity-75 text-danger'>{"."}</span>
+                    <div className='p-1 '>
+                        <span className='badge bg-dark rounded-circle text-center '>{lines}</span><p className='px-1 d-inline'>Characters</p>
+                    </div>
+
+                </div>
+
                 <p>{text.split(" ").length * 0.008} Minutes read</p>
                 <h3>Preview</h3>
-                <p>{text}</p>
+                <p className='text-wrap'>{text}</p>
             </div>
         </>
 
